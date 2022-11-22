7e6a8f65517aff85be4e4a4c7ae33af37fcac8b8
GioCC <25667790+GioCC@users.noreply.github.com>
2022-11-22 16:27:18 +0100

    Added lib files binary.h (from Arduino) and debouncer.* Archived Fast-LCD lib

---
+++ lib/Debouncer/debouncer.cpp
@@ -0,0 +1,64 @@
+/*
+* File     : debouncer.cpp
+* Version  : 1.0
+* Released : 20/08/2017
+* Author   : Giorgio CROCI CANDIANI (g.crocic@gmail.com)
+*
+*
+* Vector-level debounce filter
+*
+*/
+
+#include "debouncer.h"
+
+template <class T>
+T debouncer<T>::holder(T newi)
+{
+    unsigned long now = millis();
+    if (newi != lastValid) {    // button state changed
+        if(TlastChange == 0) {
+            TlastChange = now;  // only transitions during stable states are regarded
+        } else if (now - TlastChange >= debounceTime) {
+            TlastChange = 0;    // this condition means "stable"
+            lastValid = newi;
+        }
+    }
+    return lastValid;
+}
+
+template <class T>
+T debouncer<T>::delayer(T newi)
+{
+    unsigned long now = millis();
+    if (newi != lastValid) {    // button state changed
+        TlastChange = now;
+    } else if (now - TlastChange >= debounceTime) {
+        lastValid = newi;
+    }
+    return lastValid;
+}
+
+template <class T>
+T debouncer<T>::blanker(T newi)
+{
+    unsigned long now = millis();
+    if (now - TlastChange >= debounceTime) {
+        if (newi != lastValid) {    // button state changed
+            lastValid = newi;
+            TlastChange = now;
+        }
+    }
+    return lastValid;
+}
+
+// make the linker aware of some possible types
+template class debouncer<uint8_t>;
+//template class debouncer<int8_t>;
+template class debouncer<uint16_t>;
+//template class debouncer<int16_t>;
+template class debouncer<uint32_t>;
+//template class debouncer<int32_t>;
+//template class debouncer<uint64_t>;
+//template class debouncer<int64_t>;
+//template class debouncer<char>;
+
---
+++ lib/Debouncer/debouncer.h
@@ -0,0 +1,70 @@
+/*
+* File     : debouncer.h
+* Version  : 1.0
+* Released : 20/08/2017
+* Author   : Giorgio CROCI CANDIANI (g.crocic@gmail.com)
+*
+* Vector-level debounce filter
+*
+* Three debounce mechanisms available:
+* - holder: validates input if it has been stable for <DEBTIME_MS> ms.
+*           Guarantees that validation occurs on a stable signal, but
+*           delays detection (after the actual starting timepoint of the stable period)
+*           by <DEBTIME_MS> + the time during which the signal is not stable.
+*           Best suited when the glitch width is well known, and the delay is not an issue.
+* - delayer: validates input by taking whatever value is present <DEBTIME_MS> ms
+*           after the first transition that occurs while in a stable state.
+*           Has a delayed detection like [holder], however not longer than <DEBTIME_MS> ms
+*           (assuming the instability period is shorter than that, otherwise the returned
+*           final state might not be correct).
+*           Best suited when the max duration of the instability period is well known
+*           and the delay is not an issue.
+* - blanker: validates input right after first transition edge, ignoring any
+*           further change in the input for the next <DEBTIME_MS> ms.
+*           Has a very fast reaction time, however it is prone to being fooled
+*           by glitches which are not the start of an actual transition.
+*           Best suited when a fast reaction time is required, and no glitches are likely
+*           to occur except as a consequence of the start of an actual transition.
+*
+*/
+
+#ifndef DEBOUNCER_H
+#define DEBOUNCER_H
+
+#define DEBTIME_MS  2
+
+#include <Arduino.h>
+
+template <typename T>
+class debouncer
+{
+
+public:
+
+    debouncer()                 {debounceTime = DEBTIME_MS;}
+
+    debouncer(uint8_t dbtime)   {debounceTime = dbtime;}
+
+    T update(T value)           { return (value);}
+    T holder(T value);
+    T delayer(T value);
+    T blanker(T value);
+
+    // === Setters (single params)
+
+    void setDebounce(uint8_t dbtime)    {debounceTime = dbtime;}
+
+    // === Getters
+
+    T status(void)              {return lastValid;}
+
+private:
+
+    uint8_t         debounceTime;       // internally in ms
+    unsigned long   TlastChange;
+    T               lastValid;
+    //uint8_t         pressFlag;
+
+};
+
+#endif // DEBOUNCER_H
