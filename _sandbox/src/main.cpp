4de795848f679f1ab1d3a421e831fbc4ba36a0d7
GioCC <g.crocic@gmail.com>
2023-08-08 13:54:07 +0200

    Sandbox update
    Added ProMicro
    
    Sandbox update

--- _sandbox/src/main.cpp
+++ _sandbox/src/main.cpp
@@ -4,7 +4,7 @@
 // @project     
 //
 // @author      GiorgioCC (g.crocic@gmail.com) - 2023-07-21
-// @modifiedby  GiorgioCC - 2023-07-24 11:55
+// @modifiedby  GiorgioCC - 2023-08-08 16:18
 //
 // Copyright (c) 2023 GiorgioCC
 // =======================================================================
@@ -24,19 +24,19 @@ char sbuf[64];
 
 //Button      Btn;
 volatile ButtonAdv   BAdv;
-volatile ButtonAna   Bana;
+volatile ButtonAna   BAna;
 volatile ButtonBas   BBas;
 volatile ButtonEnc   BEnc;
-volatile ButtonGrp   BGrp;
+volatile ButtonEvt   BGrp;
 
 ButtonManager   BtnMgr;
 
 constexpr uint8_t S_Btn  = sizeof(Button);
 constexpr uint8_t S_BAdv = sizeof(ButtonAdv);
-constexpr uint8_t S_Bana = sizeof(ButtonAna);
+constexpr uint8_t S_BAna = sizeof(ButtonAna);
 constexpr uint8_t S_BBas = sizeof(ButtonBas);
 constexpr uint8_t S_BEnc = sizeof(ButtonEnc);
-constexpr uint8_t S_BGrp = sizeof(ButtonGrp);
+constexpr uint8_t S_BEvt = sizeof(ButtonEvt);
 
 constexpr uint8_t S_BtnMgr  = sizeof(ButtonManager);
 
@@ -44,10 +44,10 @@ void report(void)
 {
     sprintf(sbuf, "Size of %s: %d bytes\n\r", "Button   ", S_Btn );   Serial.println(sbuf);
     sprintf(sbuf, "Size of %s: %d bytes\n\r", "ButtonAdv", S_BAdv);   Serial.println(sbuf); 
-    sprintf(sbuf, "Size of %s: %d bytes\n\r", "ButtonAna", S_Bana);   Serial.println(sbuf);
     sprintf(sbuf, "Size of %s: %d bytes\n\r", "ButtonBas", S_BBas);   Serial.println(sbuf);
     sprintf(sbuf, "Size of %s: %d bytes\n\r", "ButtonEnc", S_BEnc);   Serial.println(sbuf);
-    sprintf(sbuf, "Size of %s: %d bytes\n\r", "ButtonGrp", S_BGrp);   Serial.println(sbuf);
+    sprintf(sbuf, "Size of %s: %d bytes\n\r", "ButtonAna", S_BAna);   Serial.println(sbuf);
+    sprintf(sbuf, "Size of %s: %d bytes\n\r", "ButtonEvt", S_BEvt);   Serial.println(sbuf);
     sprintf(sbuf, "Size of %s: %d bytes\n\r", "ButtonMgr", S_BtnMgr); Serial.println(sbuf);
     Serial.println("-------------------------\n\r");
 }
